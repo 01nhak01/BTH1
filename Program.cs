b9739a56a9a7bb6d8cb62cf7372919ec89c0db70
01nhak01 <nhak010106@gmail.com>
2025-10-07 00:07:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BTH1_Bai2
+{
+    class Program
+    {
+        //Kiểm tra số nguyên tố
+        static bool SNT(int n)
+        {
+            if (n < 2) return false;
+            for (int i = 2; i * i <= n; i++)
+                if (n % i == 0) return false;
+            return true;
+        }
+        static void Main()
+        {
+            //Nhap so nguyen duong n
+            Console.Write("Nhap so nguyen duong n: ");
+            int n = int.Parse(Console.ReadLine());
+            //Tính tổng số nguyên tố < n
+            int sum = 0;
+            for (int i = 2; i < n; i++)
+                if (SNT(i) == true) sum += i;
+            //In kết quả
+            Console.WriteLine("Tong cac so nguyen to < n la: " + sum);
+        }
+    }
+}
